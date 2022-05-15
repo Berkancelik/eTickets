84ef795ec085a0b47d7f4d5671c45b478728140d
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-15 12:04:10 +0300

    Movie and MovieCategory has been added

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using eTickets.Data;
+using System;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate{ get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
