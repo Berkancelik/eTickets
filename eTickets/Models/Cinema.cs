a646a33e1e27132ae8e8703f62d2028857dd56e6
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-15 11:59:29 +0300

    Cinema and Producer enitites has been created

---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,9 @@
+﻿namespace eTickets.Models
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,10 @@
+﻿namespace eTickets.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+        public int ProfilePictureURL { get; set; }
+        public int FullName { get; set; }
+        public int Bio { get; set; }
+    }
+}
